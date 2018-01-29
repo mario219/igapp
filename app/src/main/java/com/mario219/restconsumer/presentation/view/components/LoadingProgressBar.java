d3e8a495cf10e3dc0a5bc04c8df6847968791413
Mario Rangel <mario6.10@live.com>
2018-01-29 18:33:29 -0500

    dependencyinjections

---
+++ app/src/main/java/com/mario219/restconsumer/presentation/view/components/LoadingProgressBar.java
@@ -0,0 +1,48 @@
+package com.mario219.restconsumer.presentation.view.components;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.Window;
+
+import com.mario219.restconsumer.R;
+
+/**
+ * Created by mario on 29/01/18.
+ */
+
+public class LoadingProgressBar extends Dialog {
+
+    public LoadingProgressBar(@NonNull Context context) {
+        super(context);
+        initView();
+    }
+
+    public LoadingProgressBar(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+        initView();
+    }
+
+    protected LoadingProgressBar(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+        initView();
+    }
+
+    private void initView() {
+        hideDialogTitleBar();
+        setContentView(R.layout.view_loading_progress);
+    }
+
+    private void hideDialogTitleBar() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            Window window = getWindow();
+            if(window != null){
+                window.requestFeature(Window.FEATURE_NO_TITLE);
+            }
+        }
+    }
+
+}
